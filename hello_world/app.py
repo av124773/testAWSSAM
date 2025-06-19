6d4694345dd0b1d01ed26c96a139e25b3f7f8271
av124773 <av124773@gmail.com>
2025-06-19 15:24:15 +0800

    feat: add GSI in DynamoDB

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,23 +6,10 @@ import openai
 from botocore.exceptions import ClientError
 from datetime import datetime, timezone
 
-# --- 全域變數 ---
 OPENAI_API_KEY_SECRET_NAME = os.environ.get("OPENAI_API_KEY_SECRET_NAME")
 AWS_REGION_NAME = os.environ.get("AWS_REGION_NAME")
 
 openai_client = None
-
-# def get_secret():
-#     secret_name = "prod/testAWSSAM/test-key"
-#     region_name = "us-east-1"
-#     session = boto3.session.Session()
-#     client = session.client(service_name='secretsmanager', region_name=region_name)
-#     try:
-#         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-#     except ClientError as e:
-#         raise e
-#     secret = get_secret_value_response['SecretString']
-#     return secret
 
 def get_openai_client():
     """
