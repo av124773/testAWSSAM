f66784786e9ca3a057904eb58d539d3e9d7af540
av124773 <av124773@gmail.com>
2025-06-30 11:54:56 +0800

    fix: fix typo

--- layers/common_utils/python/common/config.py
+++ layers/common_utils/python/common/config.py
@@ -1,7 +1,7 @@
 import os
 from pydantic_settings import BaseSettings
 
-class Setting(BaseSettings):
+class Settings(BaseSettings):
     AWS_REGION_NAME: str
     DYNAMODB_TABLE_NAME: str
     OPENAI_API_KEY_SECRET_NAME: str
