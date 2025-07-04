ae8fb18c0ac7d52489957afb83a88e9a97d86967
av124773 <av124773@gmail.com>
2025-07-04 11:51:16 +0800

    refactor: remove dependencies

--- layers/common_utils/setup.py
+++ layers/common_utils/setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup, find_packages
-# import os
-
-# def parse_requirements(filename):
-#     req_path = os.path.join(os.path.dirname(__file__), filename)
-#     if not os.path.isfile(req_path):
-#         return []
-#     with open(req_path) as f:
-#         lines = [line.strip() for line in f if line.strip() and not line.startswith('#')]
-#     return lines
-
-# print(parse_requirements("requirements.txt"))
 
 setup(
     name="common_utils",
@@ -17,7 +6,6 @@ setup(
     description="Common utilities for AWS Lambda apps",
     author="kevinL",
     packages=find_packages(),
-    # install_requires=parse_requirements("requirements.txt"),
     install_requires=[
         "openai==1.92.2",
         "boto3==1.38.25",
